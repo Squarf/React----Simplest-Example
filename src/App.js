dfd97ba0b06622ed52947a21bdade5a577ee2495
Nathan Weyenberg <nathan.weyenberg@centare.com>
2018-09-04 08:24:20 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import NameForm from './NameForm.js';
+
+class App extends Component {
+  render() {
+    return (
+      <NameForm />
+    );
+  }
+}
+
+export default App;
